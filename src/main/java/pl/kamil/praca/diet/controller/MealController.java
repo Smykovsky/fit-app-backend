450da5d8e43004e7d6d7ccf9c7dafbcbbfabf6e8
Kamil Smyk <kamil.smyk00@gmail.com>
2023-02-06 15:06:35 +0100

    controllers

---
+++ src/main/java/pl/kamil/praca/diet/controller/MealController.java
@@ -0,0 +1,9 @@
+package pl.kamil.praca.diet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/meal")
+public class MealController {
+}
