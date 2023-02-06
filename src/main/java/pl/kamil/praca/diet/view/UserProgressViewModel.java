4917215025a123abf1271abc580c3f1ac2cce02d
Kamil Smyk <kamil.smyk00@gmail.com>
2023-02-06 14:56:46 +0100

    UserProgressViewModel impl

--- src/main/java/pl/kamil/praca/diet/view/UserProgressViewModel.java
+++ src/main/java/pl/kamil/praca/diet/view/UserProgressViewModel.java
@@ -1,6 +1,8 @@
 package pl.kamil.praca.diet.view;
 
 import lombok.Getter;
+import pl.kamil.praca.authentication.model.User;
+import pl.kamil.praca.diet.model.UserProgress;
 
 @Getter
 public class UserProgressViewModel {
@@ -8,4 +10,11 @@ public class UserProgressViewModel {
     private Double newWeight;
     private Double progress;
     private boolean isProgress;
+
+    public UserProgressViewModel(final UserProgress userProgress, User user) {
+        this.lastWeight = user.getWeight();
+        this.newWeight = userProgress.getNewWeight();
+        this.progress = user.getWeight() - userProgress.getNewWeight();
+        this.isProgress = (isProgress ? this.progress > 0 : this.progress < 0);
+    }
 }
