cbcee5369aa04d5d192a71e9e87ac34d49d3c628
Smyku <kamil.smyk00@gmail.com>
2023-05-19 15:34:08 +0200

    fix

--- src/main/java/pl/kamil/praca/PracaApplication.java
+++ src/main/java/pl/kamil/praca/PracaApplication.java
@@ -9,18 +9,8 @@ import org.springframework.context.event.EventListener;
 import pl.kamil.praca.emailSender.EmailSenderService;
 
 @SpringBootApplication
-@AllArgsConstructor
 public class PracaApplication {
-    private EmailSenderService emailSenderService;
     public static void main(String[] args) {
         SpringApplication.run(PracaApplication.class, args);
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void triggerMail() throws MessagingException {
-        emailSenderService.sendSimpleEmail("kamil.smyk00@gmail.com",
-                "this is email body",
-                "this is email subject"
-        );
     }
 }
