67103e60e53e5c49fb46df0e3b15e6ba00c3bb47
Smyku <kamil.smyk00@gmail.com>
2023-06-28 18:46:13 +0200

    fix

--- src/main/java/pl/kamil/praca/authentication/security/SecurityConfig.java
+++ src/main/java/pl/kamil/praca/authentication/security/SecurityConfig.java
@@ -46,13 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable();
         httpSecurity.cors().configurationSource(corsConfigurationSource()).and().csrf().disable().authorizeRequests().antMatchers("/auth/*").permitAll().anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
-            Map<String, Object> responseMap = new HashMap<>();
             ObjectMapper mapper = new ObjectMapper();
             response.setStatus(401);
             response.setHeader("content-type", "application/json");
-            responseMap.put("error", true);
-            responseMap.put("message", "Unauthorized");
-            String responseMsg = mapper.writeValueAsString(responseMap);
         }).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
