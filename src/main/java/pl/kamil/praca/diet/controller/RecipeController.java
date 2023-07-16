ae42080b86cd1f4b1fce7d6e9f417805908447bb
Smyku <kamil.smyk00@gmail.com>
2023-07-16 14:29:50 +0200

    fix

--- src/main/java/pl/kamil/praca/diet/controller/RecipeController.java
+++ src/main/java/pl/kamil/praca/diet/controller/RecipeController.java
@@ -12,7 +12,13 @@ import pl.kamil.praca.diet.model.Recipe;
 import pl.kamil.praca.diet.service.RecipeService;
 
 import javax.validation.Valid;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,10 +40,29 @@ public class RecipeController {
         if (authentication == null || !authentication.isAuthenticated()) {
             return ResponseEntity.status(403).body("Użytkownik nie jest zautoryzowany!");
         }
+        String path = "C:\\Users\\Smyku\\Desktop\\praca\\praca-frontend\\static\\images";
 
-        Recipe recipe = recipeService.getRecipe(id);
-        recipe.setPhotoUrl(file.getOriginalFilename());
-        recipeService.save(recipe);
+        try {
+            Path uploadPath = Paths.get(path);
+            if (!uploadPath.toFile().exists()) {
+                uploadPath.toFile().mkdirs();
+            }
+
+            UUID randomName = UUID.randomUUID();
+            Path filePath = uploadPath.resolve(String.valueOf(randomName) + ".jpg");
+            FileOutputStream fos = new FileOutputStream(filePath.toFile());
+            fos.write(file.getBytes());
+            fos.close();
+
+            Recipe recipe = recipeService.getRecipe(id);
+            recipe.setPhotoUrl(randomName + ".jpg");
+            recipeService.save(recipe);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return ResponseEntity.ok("Dodano!");
     }
 
