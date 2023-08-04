ac23261371fab130d86ea5159207cac08d59495b
Smyku <kamil.smyk00@gmail.com>
2023-08-04 10:52:09 +0200

    fix mailsender

--- src/main/java/pl/kamil/praca/PracaApplication.java
+++ src/main/java/pl/kamil/praca/PracaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Properties;
+
 @SpringBootApplication
 @EnableScheduling
 public class PracaApplication {
@@ -17,6 +19,17 @@ public class PracaApplication {
 
     @Bean
     JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("ksmyk.dev2000@gmail.com");
+        mailSender.setPassword("umijnrbhxagjqlmd");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        return mailSender;
     }
 }
